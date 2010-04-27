7c150ba949ae2d307a9afc412368c0a93d97f782
Gavin Heavyside <gavin@heavyside.co.uk>
2010-04-27 20:37:26 +0100

    add lein-swank as a dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject sicp "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [leiningen/lein-swank "1.2.0-SNAPSHOT"]])
 
